5b0dc85fd42eff502b1d069b4c3bb322d65cac37
kalandhar26 <babakalandhar499@gmail.com>
2023-07-04 13:55:38 +0530

    first commit

---
+++ microserviceparent/src/main/java/com/example/microserviceparent/MicroserviceparentApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceparentApplication.class, args);
+	}
+
+}
